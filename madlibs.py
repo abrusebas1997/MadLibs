49786e1b7c2c78ae55ab6f16a240172059975d86
Sebastian Abarca <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-09-03 11:21:28 -0700

    Finished

---
+++ madlibs.py
@@ -0,0 +1,41 @@
+# Here I used import random, so i can use it when I print my text
+import random
+# Here I added the color magenta in the output
+import sys
+sys.stdout.write("\033[0;35m")
+print("Mad Libs!")
+print("Enter an example of each")
+0
+#Here I used a for loop to make my code much more organized
+adjective = []
+i = 0
+max_adjectives = 5
+while i < max_adjectives:
+      adjective.append(input("Enter a random adjective: "))
+      i += 1
+
+
+verb = []
+verb.append(input("Enter a random verb1: "))
+verb.append(input("Enter a random verb2: "))
+
+
+noun = []
+i = 0
+max_nouns = 4
+while i < max_nouns:
+      reply = input("Enter a random noun: ")
+      while len(reply) < 1:
+            print("You got to write something! ")
+            reply = input("Enter a random noun: ")
+      noun.append(reply)
+
+
+
+      i += 1
+
+
+
+
+# I borrowed this mad lib from https://me.me/i/madolibs-print-our-cafeteria-glamorous-adjective-our-school-cafeteria-has-c4aa33f3592a4a8b910a2e4c89472e01
+print("Our school cafeteria has really", random.choice(adjective), "food. Just thinking about it makes my stomach", random.choice(verb), ". The spaguetti is", random.choice(adjective),"and tastes like", random.choice(noun),". One day, I swear one of my meatballs started to", random.choice(verb), "!The turkey tacos are totally", random.choice(adjective), "and look kind of like old", random.choice(noun),". My friend Dana actually likes the meatloaf, even though it's", random.choice(adjective), "and", random.choice(adjective), ".I call it 'mystery meatloaf' and think it's really made out of", random.choice(noun), ".My dad said he'd make my lunches, but the first day, he made me a sandwich out of", random.choice(noun), "and peanut butter! I think I'd rather take my chances with the cafeteria!")
